b21207aa161b98f8572b5ef01ee53408c8bc1b14
bexb0t <chrysslyn@gmail.com>
2024-08-20 00:12:57 -0700

    boilerplate spring boot project with starter-web

---
+++ src/test/java/com/bex/cpi_search/CpiSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bex.cpi_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpiSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
